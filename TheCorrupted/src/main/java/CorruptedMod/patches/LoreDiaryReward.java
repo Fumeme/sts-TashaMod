9194a26eeca760d5127c9a9d0484ed5eb434cbbf
SacredDiamond <45544974+SacredDiamond@users.noreply.github.com>
2019-02-08 12:02:48 -0500

    Update LoreDiaryReward.java

--- TheCorrupted/src/main/java/CorruptedMod/patches/LoreDiaryReward.java
+++ TheCorrupted/src/main/java/CorruptedMod/patches/LoreDiaryReward.java
@@ -10,6 +10,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import CorruptedMod.CorruptedBase;
 
@@ -19,20 +20,23 @@ public class LoreDiaryReward extends CustomReward {
     public LoreDiaryReward() {
         super(ICON, "Lore Card Reward", LoreDiaryPatch.LoreDiaryReward);
 
-    }
-
-    @Override
-    public boolean claimReward() {
     	ArrayList<AbstractCard> LoreList = new ArrayList<AbstractCard>();
 
     	
     	for (AbstractCard c : CardLibrary.getAllCards()) {
-    	if(c.hasTag(CorruptedBase.Lore)) LoreList.add(c);
+    	if(c.hasTag(CorruptedBase.Lore)) LoreList.add(c.makeStatEquivalentCopy());
     	}
     	
         AbstractDungeon.player.getCardPool(LoreList);
         
         this.cards = LoreList;
+        /*                                     */
+
+
+    }
+
+    @Override
+    public boolean claimReward() {
 
 
     		if(AbstractDungeon.screen == AbstractDungeon.CurrentScreen.COMBAT_REWARD) {
